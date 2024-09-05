6d77ed5c5d8691ab0591a8be46100f9d519e1cea
alexgburnet <alexburnet03@gmail.com>
2024-09-05 16:32:23 +0100

    Starting to implement firebase

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,12 @@
-import { View, Text, SafeAreaView, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, StyleSheet, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { Link, router } from 'expo-router'
 
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 
-
+import { firebase } from '../../firebaseConfig'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';  // Updated import for Firebase v9+
 
 /**
  * Screen to handle sign in
@@ -19,6 +20,24 @@ const signIn = () => {
     email: '',
     password: '',
   })
+
+  const auth = getAuth();
+
+  const handleLogin = () => {
+    const { email, password } = form;
+
+    if (email && password) {
+      signInWithEmailAndPassword(auth, email, password)  // Use Firebase v9+ method
+        .then((userCredential) => {
+          router.replace('/home'); 
+        })
+        .catch((error) => {
+          Alert.alert('Login Failed', error.message);
+        });
+    } else {
+      Alert.alert('Error', 'Please fill out both email and password.');
+    }
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -44,7 +63,7 @@ const signIn = () => {
       <View style={styles.signin}>
         <CustomButton 
           title="Sign-in"
-          handlePress={() => {router.replace('/home')}}
+          handlePress={handleLogin}
         />
         <Text style={styles.bottomtext}>Dont have an account? <Text style={styles.link} onPress={() => {router.push('about-you')}}>Sign Up</Text></Text>
       </View>
